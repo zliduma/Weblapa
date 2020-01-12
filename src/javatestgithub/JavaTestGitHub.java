928cff188dd337f18589b7de42abfb0ad9c884ea
nitobenito <nito.benito@inbox.lv>
2020-01-12 17:27:42 +0200

    pirmais

--- src/javatestgithub/JavaTestGitHub.java
+++ src/javatestgithub/JavaTestGitHub.java
@@ -16,7 +16,7 @@ public class JavaTestGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("vel viens eginajums");
+        System.out.println("vel viens meeginajums");
     }
     
 }
